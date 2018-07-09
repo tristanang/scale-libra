dde8a836c7adb6c8ce4563c8e595e3d04df058e9
Tristan Ang Tze Heng <theng@scalinewheights.local>
2018-07-09 16:25:10 -0400

    end of day commit

---
+++ scale-libra/immutableDeviceInfo.py
@@ -0,0 +1,18 @@
+__device_info_dict = {}
+__lst = ('pdu','switch','server')
+
+for device in __lst:
+	__device_info_dict = {}
+
+__ports = ('ethernet_port','power_port')
+
+__device_info_dict['pdu'][] =
+__device_info_dict['pdu'] =
+
+
+class Device:
+	def __init__(self):
+
+	def __eq__(self,other):
+
+	def __hash__(self):
